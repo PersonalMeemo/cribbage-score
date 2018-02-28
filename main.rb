b46ddf3a15669ae5cd11ba3d897b25efeee81fa8
Vanessa Nimmo <vanessanimmo@live.com>
2018-02-28 16:15:13 +1100

    Update main.rb

--- main.rb
+++ main.rb
@@ -3,6 +3,7 @@
 #method declarations
 
 #methods to deal with the data
+
 def read_file_to_array
 	File.readlines(ARGV.first).to_s
 end
@@ -11,7 +12,6 @@ def slice_string(input)
 	input.slice(2,14).split(/,/)
 end
 
-#create array of just the numbers, substituting for face cards to make them sortable
 def make_numbers_array(cards)
 	card_nums = cards.collect {|card| card.chop}
 	a = card_nums.collect do |card|
@@ -31,8 +31,19 @@ def make_numbers_array(cards)
 	a.sort!
 end
 
+#first converts face cards to make numerical scoring simple
+def remove_face_cards(a)
+	a_15s = a.collect do |card|
+		if card == 11 || card == 12 || card == 13
+			card = 10
+		else
+			card = card
+		end
+	end
+end
+
 #methods to calculate the score
-#check for Nobs 
+ 
 def check_for_Nobs(input)
 	nobs_score = 0
 	jack_suit = input[15]
@@ -50,7 +61,6 @@ def check_for_Nobs(input)
 	nobs_score
 end
 
-#finding multiples
 def finding_multiples
 	multiples_score = 0
 	unique = a.uniq
@@ -72,78 +82,77 @@ end
 
 
 #find consecutive cards
-a.collect do |card|
-	if a.include? (card+1)
-		if a.include? (card+2)
-			if a.include? (card+3)
-				if a.include? (card+4)
-					score+=5
+def find_consecutives
+	consecutives_score = 0
+	a.collect do |card|
+		if a.include? (card+1)
+			if a.include? (card+2)
+				if a.include? (card+3)
+					if a.include? (card+4)
+						consecutives_score+=5
+					else
+						consecutives_score+=4
+					end
 				else
-					score+=4
+					consecutives_score+=3
 				end
-			else
-				score+=3
 			end
 		end
 	end
+	consecutives_score
 end
-puts "Score after checking for runs is " + score.to_s
 
-#finds combinations that add up to 15
+def calculate_15s(a_15s)
+	fifteens_score=0
+	combos_2 = a_15s.combination(2).to_a
+	combos_3 = a_15s.combination(3).to_a
+	combos_4 = a_15s.combination(4).to_a
+	combos_2.collect do |combo|
+		if combo.inject(:+) == 15
+			fifteens_score += 2
+		end
+	end
+	combos_3.collect do |combo|
+		if combo.inject(:+) == 15
+			fifteens_score += 2
+		end
+	end
+	combos_4.collect do |combo|
+		if combo.inject(:+) == 15
+			fifteens_score += 2
+		end
+	end
+	fifteens_score
+end
 
-#first converts face cards to make numerical scoring simple
-a_15s = a.collect do |card|
-	if card == 11 || card == 12 || card == 13
-		card = 10
+def compare_suits
+	suits_score = 0
+	card_suit = cards.collect {|card| card.slice(1)}
+	if card_suit.length-card_suit.uniq.length == 4
+		suits_score+=5
 	else
-		card = card
+		card_suit.pop
+		if card_suit.length-card_suit.uniq.length == 3
+			suits_score+=4
+		end
 	end
+	suits_score
 end
-
-#then calculates combinations and sums each option
-combos_2 = a_15s.combination(2).to_a
-combos_3 = a_15s.combination(3).to_a
-combos_4 = a_15s.combination(4).to_a
-combos_2.collect do |combo|
-	if combo.inject(:+) == 15
-		score += 2
-	end
-end
-combos_3.collect do |combo|
-	if combo.inject(:+) == 15
-		score += 2
-	end
-end
-combos_4.collect do |combo|
-	if combo.inject(:+) == 15
-		score += 2
-	end
-end
-
-puts "Score after checking for 15s is " + score.to_s
-
-#finds cards of same suit
-card_suit = cards.collect {|card| card.slice(1)}
-if card_suit.length-card_suit.uniq.length == 4
-	score+=5
-else
-	card_suit.pop
-	if card_suit.length-card_suit.uniq.length == 3
-		score+=4
-	end
-end
-
-puts "Score after checking for cards of same suit is " + score.to_s
-
-puts "Final score is " + score.to_s
 
 #program starts here
 input = read_file_to_array
 score = 0
 cards = slice_string(input)
 numbers = make_numbers_array(cards)
+no_face_cards = remove_face_cards(numbers)
 score+=check_for_Nobs(input)
 puts "Score after Nobs check is " + score.to_s
 score+=multiples_score
 puts "Score after checking for pairs is " + score.to_s
-
+score+=find_consecutives
+puts "Score after checking for runs is " + score.to_s
+score+=calculate_15s(numbers)
+puts "Score after checking for 15s is " + score.to_s
+score+=suits_score(cards)
+puts "Score after checking for cards of same suit is " + score.to_s
+puts "Final score is " + score.to_s
